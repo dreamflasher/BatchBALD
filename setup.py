d6511127c843f3059f60d7b4427daf4f162f3fef
Marcel Ackermann <31695+dreamflasher@users.noreply.github.com>
2020-03-16 13:16:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup
 setup(name='batchbald',
       description='BatchBALD',
       url='https://github.com/BlackHC/BatchBALD/',
-      packages=['src'],
+      packages=['.'],
       install_requires=[line.rstrip('\n') for line in open("requirements.txt")],
       zip_safe=True)
