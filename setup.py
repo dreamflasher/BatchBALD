3e2f3d1e0e27c9f1c502bf1e52fa3df154e2c4e0
Marcel Ackermann <31695+dreamflasher@users.noreply.github.com>
2020-03-16 13:54:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup
 setup(name='BatchBALD',
       description='BatchBALD',
       url='https://github.com/BlackHC/BatchBALD/',
-      packages=['BatchBALD'],
+      packages=['BatchBALD', 'BatchBALD/joint_entropy'],
       install_requires=[line.rstrip('\n') for line in open("requirements.txt")],
       zip_safe=True)
